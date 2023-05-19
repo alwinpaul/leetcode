4d1adeff79845c5bf2905fc46c288e73b5fe3648
Alwin Paul <alwin.paul7489@gmail.com>
2023-05-19 11:39:21 -0400

    valid sudoku

---
+++ arrays/validSudoku.js
@@ -0,0 +1,107 @@
+/** 
+ * 36. Valid Sudoku
+ * 
+ 
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+
+
+ * 
+ * 
+*/
+
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+
+    let rows = initRC();
+    let cols = initRC();
+    let boxes = initBoxes();
+
+    for (let i=0; i<board.length; i++) {
+        for (let j=0; j<board.length; j++) {
+            let v = board[i][j]
+            if( v === '.') {
+                continue;
+            }
+            let bx = `${parseInt(i/3)}${parseInt(j/3)}`
+
+            if (rows.get(i).includes(v) ||
+                cols.get(j).includes(v) ||
+                boxes.get(bx).includes(v)
+            ) {
+                return false
+            }
+
+            rows.get(i).push(v)
+            cols.get(j).push(v)
+            boxes.get(bx).push(v)
+        }
+    }
+    return true;
+};
+
+function initRC() {
+    let rc = new Map();
+    for(let i=0; i<9; i++) {
+        rc.set(i, [])
+    }
+    return rc
+}
+
+function initBoxes() {
+    let boxes = new Map();
+    for(let i=0; i<3; i++) {
+        for(let j=0; j<3; j++) {
+            boxes.set(`${i}${j}`, [])
+        }
+    }
+    return boxes
+}
